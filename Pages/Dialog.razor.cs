8378bf83634dbb18dad7ee3400f53fbe9e3fabea
Rajesh Singh <jss.rksingh@hotmail.com>
2020-04-11 14:07:43 +0530

    Commit locall changes.

---
+++ Pages/Dialog.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp_ClientPortal.Pages
+{
+    public partial class Dialog
+    {
+        [Parameter]
+        public string Title { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        private void OnYes()
+        {
+            Title = "OnYes button is clicked!";
+        }
+    }
+}
