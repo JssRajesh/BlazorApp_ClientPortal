60bd0993284d5d1b3ae674edb7e882cdb27bc6b6
Rajesh Singh <jss.rksingh@hotmail.com>
2020-04-15 15:52:54 +0530

    Forms and Validations.

---
+++ Models/ExampleModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp_ClientPortal.Models
+{
+    public class ExampleModel
+    {
+        [Required]
+        [MaxLength(5, ErrorMessage = "Length can not be more than 5 chars")]
+        public string Name { get; set; }
+    }
+}
